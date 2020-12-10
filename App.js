30a41a8b20c636cdca572c1d7ece9d224b47c11a
Irene <irene_lee@live.com.sg>
2020-12-10 12:55:52 +0800

    Added stack and title

--- App.js
+++ App.js
@@ -2,17 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+
+
+function NotesScreen({navigation}) {
+  return <View style={styles.container}></View>
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
 
-  
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Notes"
+        component={NotesScreen}
+        options={{
+          headerTitle: "Notes App",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 30,
+          },
+          headerStyle: {
+            height: 120,
+            backgroundColor: "yellow",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+
   );
 }
 
