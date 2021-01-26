644b6e17b30b6f387dadfe0620b8b7aea97dc72d
Irene-miao <71961907+Irene-miao@users.noreply.github.com>
2021-01-26 16:02:29 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,50 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as SQLite from 'expo-sqlite';
-import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-
-
-function NotesScreen({navigation}) {
-  return <View style={styles.container}></View>
-}
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AddScreen from "./screens/AddScreen";
+import NotesStack from "./screens/NotesStack";
 
 const Stack = createStackNavigator();
-
 export default function App() {
   return (
-
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Notes"
-        component={NotesScreen}
-        options={{
-          headerTitle: "Notes App",
-          headerTitleStyle: {
-            fontWeight: "bold",
-            fontSize: 30,
-          },
-          headerStyle: {
-            height: 120,
-            backgroundColor: "yellow",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-          },
-        }}
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
-
+      <Stack.Navigator headerMode="none" mode="modal">
+        <Stack.Screen name="Notes Stack" component={NotesStack} />
+        <Stack.Screen name="Add Screen" component={AddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
